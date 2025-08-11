2a213fb109411cd193863d55f7288d6b8071ffeb
Hemapriya1111 <hemapriya.yoho@gmail.com>
2025-08-11 19:33:19 +0530

    Layout-PMS

--- src/constants/ui constants.tsx
+++ src/constants/ui constants.tsx
@@ -1,36 +1,41 @@
 export const COLORS={
     primary: '#B200FF' // for add , view buttons
 }
-export const headers={
+export const FONTS={
+    headers:{
     fontfamily:'Afacad', //for all page header contents 
     font:'Bold',
     fontweight:'700',
     size:'24px',
 
-}
-export const subheaders={
+},
+ subheaders:{
      fontfamily:'Afacad', //for all page sub headers like table sub headers 
     font:'SemiBold',
     fontweight:'600',
     size:'18px',
-}
-export const paragraph={
+},
+  paragraph:{
      fontfamily:'Afacad', //for all pages small texts, like inside table, model, .. 
     font:'Bold',
     fontweight:'700',
     size:'18px',
     color:'#7D7D7D'
 
-}
-export const models={
+},
+ models:{
  fontfamily:'Afacad', //for all page model headers 
     font:'SemiBold',
     fontweight:'600',
-    size:'24px',
+    fontsize:'24px',
     text:'#000000',
-    subtext:'#7D7D7D',//for sub headings inside models
-    Fontfamily:'Afacad',
-    Font:'SemiBold',
-    Fontweight:'600',
-    Size:'24px',
+
+},
+ subheadsmodels:{
+    text:'#7D7D7D',//for sub headings inside models
+    fontfamily:'Afacad',
+    font:'SemiBold',
+    fontweight:'600',
+    fontsize:'24px',
+}
 }
