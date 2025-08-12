37ffc389d72ed650a08185cf3ca0189b5ea67300
Prakash-yoho <prakash.yohotechnologies@gmail.com>
2025-08-12 12:41:48 +0530

    Bg-Frames

--- src/pages/financial reports/FinancialReports.tsx
+++ src/pages/financial reports/FinancialReports.tsx
@@ -4,7 +4,7 @@ import { FONTS } from '../../constants/ui constants'
 function FinancialReports() {
   return (
     <div>
-      <div className='bg-amber-200'>
+      <div className='bg-amber-200 flex justify-between p-2'>
         <section>
           <h1 style={{...FONTS.headers}}>Financial Reports & Analytics</h1>
           <p>Comprehensive insights into your property portfolio</p>
