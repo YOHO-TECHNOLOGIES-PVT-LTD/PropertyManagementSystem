49ded8f7f8c385ac383f4a4eb055ce85c4469ffb
Abishekyoho <abish.yoho@gmail.com>
2025-08-13 10:31:07 +0530

    Dashboard completed

--- src/components/dashboard/RecentActivity&task/Activity&task.tsx
+++ src/components/dashboard/RecentActivity&task/Activity&task.tsx
@@ -36,11 +36,11 @@ export default function ActivityTabs({
   const getStatusBadgeStyles = (status: string) => {
     switch (status) {
       case "Completed":
-        return "bg-green-100 text-green-700 border border-green-200";
+        return "bg-[#1CAF191A]/10 text-[#1CAF19] border border-[#1CAF19]";
       case "Pending":
-        return "bg-yellow-100 text-yellow-700 border border-yellow-200";
+        return "bg-[#FFC3001A]/10 text-[#FFC300] border border-[#FFC300]";
       case "Urgent":
-        return "bg-red-100 text-red-700 border border-red-200";
+        return "bg-[#E212691A] text-[#E21269] border border-[#E21269]";
       default:
         return "bg-gray-100 text-gray-700 border border-gray-200";
     }
@@ -49,11 +49,11 @@ export default function ActivityTabs({
   const getPriorityBadgeStyles = (priority: string) => {
     switch (priority) {
       case "Low":
-        return "bg-green-100 text-green-700 border border-green-200";
+        return "bg-[#1CAF191A]/10 text-[#1CAF19] border border-[#1CAF19]";
       case "Medium":
-        return "bg-yellow-100 text-yellow-700 border border-yellow-200";
+        return "bg-[#FFC3001A]/10 text-[#FFC300] border border-[#FFC300]";
       case "High":
-        return "bg-red-100 text-red-700 border border-red-200";
+        return "bg-[#E212691A] text-[#E21269] border border-[#E21269]";
       default:
         return "bg-gray-100 text-gray-700 border border-gray-200";
     }
@@ -62,13 +62,13 @@ export default function ActivityTabs({
   const getIconStyles = (icon: string) => {
     switch (icon) {
       case "pink":
-        return "bg-pink-100 text-pink-600";
+        return "bg-[#B200FF26]/10 text-[#B200FF]";
       case "blue":
-        return "bg-blue-100 text-blue-600";
+        return "bg-[#006AFF26]/10 text-[#006AFF]";
       case "red":
-        return "bg-red-100 text-red-600";
+        return "bg-[#FF003C26]/10 text-[#FF003C]";
       case "yellow":
-        return "bg-yellow-100 text-yellow-600";
+        return "bg-[#FF990026]/15 text-[#FF9900]";
       default:
         return "bg-gray-100 text-gray-600";
     }
