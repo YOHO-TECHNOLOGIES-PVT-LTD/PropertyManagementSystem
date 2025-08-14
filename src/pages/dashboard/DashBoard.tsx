086fb68e9e0a7e9cc2ad1f3f9f55b1aa359aa31c
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2025-08-14 18:12:38 +0530

    completed rent management

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -13,9 +13,10 @@ import ActivityTabs from "../../components/dashboard/RecentActivity&task/Activit
 import RadialChart from "../../components/dashboard/PaymentCharts/RadicalChart";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch } from "../../store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { DashboardThunks } from "../../features/Dashboard/Reducer/DashboardThunk";
 import { selectDashboardData } from "../../features/Dashboard/Reducer/Selector";
+import LoadingOverlay from "../../components/Loading/Loading";
 
 // Types
 export interface PropertyTotal {
@@ -129,11 +130,24 @@ const sampleTaskData = [
 
 const DashBoard = () => {
   const dispatch = useDispatch<AppDispatch>();
+   const [loading, setLoading] = useState(true);
   const dashboardData = useSelector(selectDashboardData);
 
+ 
   useEffect(() => {
     dispatch(DashboardThunks());
+
+   
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
   }, [dispatch]);
+
+  if (loading) {
+    return <LoadingOverlay />; 
+  }
 
   const totalProperties =
     dashboardData?.PropertiesTotal?.reduce(
@@ -151,7 +165,7 @@ const DashBoard = () => {
       fill: ["#E800DC", "#006AFF", "#FF008C"][index % 3],
     })) || [];
 
-  const rentCollectionData = dashboardData?.rentCollectionGraph?.map((item) => {
+  const rentCollectionData = dashboardData?.data?.rentCollectionGraph?.map((item: any) => {
       const monthName = new Date(
         item._id.year,
         item._id.month - 1
@@ -177,6 +191,7 @@ const DashBoard = () => {
 
   return (
     <div className="p-3 flex flex-col gap-6">
+       
       {/* Header */}
       <div className="flex justify-between items-center">
         <div>
